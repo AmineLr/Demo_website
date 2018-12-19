4e8fd5d972e441d52ff4feb7ac37aab96e483e0f
AmineLr <40025736+AmineLr@users.noreply.github.com>
2018-12-19 23:28:38 +0100

    Udpate home.php
    Commentaire déplacé dans le ReadMe

--- view/home.php
+++ view/home.php
@@ -12,14 +12,6 @@
 	<?php
 
 
-		/* Voilà mon site , un blog pour un ecrivain qui souhaiterai poster mensuellement des chapitres de son ouvrage.
-		
-		 Il n'y a qu'un bouton utilisant js en essayant de faire plus compliqué pour le moment je faisais tout bugger (je suis novice en js)
-
-		 Il ne contient pas non plus de CSS pour cela je vous laisse voir l'autre repo
-		 en m'excusant pour ce commentaire un peu trop long */
-
-
 	foreach ($chapitres as $chapitre )
 	{
 
